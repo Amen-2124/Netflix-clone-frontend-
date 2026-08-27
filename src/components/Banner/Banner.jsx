1d7ef50e037b8159ca0c8fda5d01e94d6fb3e484
Dawit052179 <gddave04@gmail.com>
2026-08-27 00:45:34 -0400

    Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -20,16 +20,13 @@ function Banner() {
       const request = await MovieInstance.get(requests.fetchNetflixOriginals);
       setBannerImage(
         request.data.results[
-          Math.floor(Math.random()* request.data.results.length)],
+          Math.floor(Math.random() * request.data.results.length)
+        ],
       );
     }
     fetchBannerImage();
   }, []);
-   // console.log(bannerImage)
-
-
-    
-
+  // console.log(bannerImage)
 
   return (
     <div
@@ -50,7 +47,7 @@ function Banner() {
         <h1 className={styles.title}>{bannerImage?.original_name}</h1>
         {/* description */}
         <h1 className={styles.description}>
-          {truncate(bannerImage?.overview,120)}
+          {truncate(bannerImage?.overview, 120)}
         </h1>
         {/* buttons */}
         <div className={styles.buttonContainer}>
@@ -72,67 +69,3 @@ function Banner() {
 //import { Form } from "react-router-dom";
 
 export default Banner;
-
-
-
-
-// import React, { useState, useEffect } from "react";
-// import netflixBannerLogo from "../../assets/image/logo.png";
-// import { Play, Info } from "lucide-react";
-// import styles from "./Banner.module.css";
-// import MovieInstance from "../../Utility/MovieInstance";
-// import requests from "../../Utility/RequestUrls";
-
-// const BANNER_BASE = "https://image.tmdb.org/t/p/original/";
-
-// function Banner() {
-//   const [bannerImage, setBannerImage] = useState({});
-
-//   useEffect(() => {
-//     async function fetchBannerImage() {
-//       const request = await MovieInstance.get(requests.fetchNetflixOriginals);
-//       setBannerImage(
-//         request.data.results[
-//           Math.floor(Math.random() * request.data.results.length)
-//         ],
-//       );
-//     }
-//     fetchBannerImage();
-//   }, []);
-
-//   return (
-//     <div
-//       className={styles.Banner}
-//       style={{
-//         backgroundSize: "cover",
-//         backgroundImage: `url("${BANNER_BASE}${bannerImage?.backdrop_path || ""}")`,
-//         backgroundPosition: "center center",
-//       }}
-//     >
-//       <div className={styles.contents}>
-//         <img
-//           className={styles.logoImg}
-//           src={netflixBannerLogo}
-//           alt="Netflix logo"
-//         />
-//         <h1 className={styles.title}>
-//           {bannerImage?.title ||
-//             bannerImage?.name ||
-//             bannerImage?.original_name}
-//         </h1>
-//         <h1 className={styles.description}>{bannerImage?.overview}</h1>
-//         <div className={styles.buttonContainer}>
-//           <button className={styles.button}>
-//             <Play size={30} /> Play
-//           </button>
-//           <button className={styles.button}>
-//             <Info size={30} /> My List
-//           </button>
-//         </div>
-//       </div>
-//       <div className={styles.fadeBottom}></div>
-//     </div>
-//   );
-// }
-
-// export default Banner;
